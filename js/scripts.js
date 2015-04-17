760ad0cb0a74bad89c76e2a0dd295901355a4b45
Daniel Toader <daniel.t.pdx@gmail.com>
2015-04-17 12:23:50 -0700

    refactor

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// to Diane: since we were talking about constructors for object prototypes right before the asssement, I figured that since there's a 1001 ways to do things in JavaScript, there must be a cleaner way. Here's what I came up with. I understand how to do it with Object.create and how to create an init function inside, but to me this way is simpler and cleaner and evidently more compatible with old browsers according some posts on StackExchange. I'm using the "new" keyword to instantiate new instances of Client. It's visualy very similar to Java and PHP, so that's probably why it looks prettier to me. Anyways, I blew most of the morning trying to figure this out, so please forgive the blah front-end.
+var allClients = [];
 
 function Client() {
   this.name = "";
@@ -14,6 +14,14 @@ function Client() {
   };
 }
 
+function getMessage(client) {
+  var message = client.cheese > 0 ? client.cheese + " cheese" : "";
+  message += client.cheese > 0 && client.pepperoni > 0 ? " and " : "";
+  message += client.pepperoni > 0 ? client.pepperoni + " pepperoni" : "";
+  message += client.cheese + client.pepperoni > 1 ? " pizzas" : " pizza";
+
+  return message;
+}
 
 jQuery(document).ready(function() {
   $("#name").focus();
@@ -37,17 +45,18 @@ jQuery(document).ready(function() {
       newClient.addPizza("cheese", cheese);
       newClient.addPizza("pepperoni", pepperoni);
 
-      var message = cheese > 0 ? cheese + " cheese" : "";
-      message += cheese > 0 && pepperoni > 0 ? " and " : "";
-      message += pepperoni > 0 ? pepperoni + " pepperoni" : "";
-      message += cheese + pepperoni > 1 ? " pizzas" : " pizza";
+      allClients.push(newClient);
+
+      var message = getMessage(newClient);
 
       $("#order").text(message);
       $("#message").show();
 
       $("#new-order").click(function(event) {
         $("#message").hide();
+        $("input").val("");        
         $("#name-form").show();
+        $("#name").focus();
       });
     });
   });
